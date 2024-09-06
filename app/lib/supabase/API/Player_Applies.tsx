f25ec98ebf85e472f611f9da03bbaa197a0b6082
ToCoMi-P <73607964+ToCoMi-P@users.noreply.github.com>
2024-09-06 19:26:47 +0200

    - bugfix Date doesnt work yet

--- app/lib/supabase/API/Player_Applies.tsx
+++ app/lib/supabase/API/Player_Applies.tsx
@@ -1,6 +1,7 @@
 import supabase from "@/app/lib/supabase/supabase_api";
 import {list} from "postcss";
 import {PostgrestSingleResponse} from "@supabase/supabase-js";
+import {getNextMonday} from "@/app/__lib/Date";
 
 const rest_endpoint: string = 'Player_Applies'
 
@@ -15,14 +16,16 @@ export default class Player_Applies {
 
     static async addPlayerApplies(listOfPlayers: any){
         let res: any[] = []
+        const now = new Date()
+
         listOfPlayers.forEach((playerId: string) => {
-            res.push({date: '2024-09-02', apply_timestamp: '2024-08-29 09:13:00', player_id: playerId})
+            res.push({date: getNextMonday(now), apply_timestamp: `${now.getFullYear()}-${now.getMonth()+1}-${now.getDay()+1} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`, player_id: playerId})
         })
-        supabase.from(rest_endpoint).insert(res)
         return supabase.from(rest_endpoint).insert(res)
     }
 
     static async removePlayerApply(playerId: number){
+        console.log(playerId)
         return supabase.from(rest_endpoint).delete().eq("player_id", playerId)
     }
 
